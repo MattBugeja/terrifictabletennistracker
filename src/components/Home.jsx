16b8d3c66fd6770e1ae55f50b65a93c932f86859
mattBugeja <matthewabugeja@gmail.com>
2024-10-18 14:32:31 +0200

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
+
 const Home = () => {
   return (
     <div className=" flex justify-center items-center h-screen p-4">
       <div className="text-black font-semibold text-xl">
         Page intentionally left blank (for now) - If you want to know when it's
         updated feel free to add me on
-        <a className="text-blue-600 pl-1" href="www.linkedin.com/in/matthewabugeja">
+        <a className="text-blue-600 pl-1" href="https://www.linkedin.com/in/matthewabugeja">
           LinkedIn
         </a>
       </div>
