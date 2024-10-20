9ff5a535719707579ac8f8783dc190c4b5c7dbe3
mattBugeja <matthewabugeja@gmail.com>
2024-10-20 05:46:00 +0200

    Test

--- src/main.jsx
+++ src/main.jsx
@@ -12,21 +12,22 @@ import Home from './components/Home.jsx';
 import ErrorPage from './error-page.jsx';
 import TableTop from './components/TableTop.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/TerrificTableTennisTracker",
-    element: <TableTop />,
-    errorElement: <ErrorPage />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//     errorElement: <ErrorPage />,
+//   },
+//   {
+//     path: "/TerrificTableTennisTracker",
+//     element: <TableTop />,
+//     errorElement: <ErrorPage />,
+//   },
+// ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <div>Hello</div>
     </StrictMode>,
 )
